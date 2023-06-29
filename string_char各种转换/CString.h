e700537c6af1949076ad5e76720767cf95c9f8e7
liping <liping@trawe.cn>
2023-06-29 17:15:42 +0800

    添加CString的操作

---
+++ string_char各种转换/CString.h
@@ -0,0 +1,10 @@
+/*
+ * @Description: CString学习
+				CString 是 MFC（Microsoft Foundation Classes）库中的一种字符串类。
+				它是一个用于处理字符数据的类，提供了许多方便的方法和操作符来操作字符串。
+ * @Author: Ivy
+ * @Date: 2023-06-29 09:34:43
+ * @LastEditTime: 2023-06-29 11:28:29
+ * @LastEditors: XTZJ-2022OQEHLZ
+ */
+
