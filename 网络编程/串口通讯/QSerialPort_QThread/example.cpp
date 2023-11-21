0ac5fbde3103297229a724c77f30964222ce8c4d
liping <liping@trawe.cn>
2023-11-21 09:48:54 +0800

    更新串口类的使用说明

--- 网络编程/串口通讯/QSerialPort_QThread/example.cpp
+++ 网络编程/串口通讯/QSerialPort_QThread/example.cpp
@@ -8,6 +8,13 @@ m_SerialThread = new SerialThread();
 m_SerialThread->start();
 
 
-//绑定一些信号槽
+//3、绑定一些信号槽
 connect(m_SerialThread, &SerialThread::sigRecvOneFrame, this, &XXX::slotRecvOneFrame);
 connect(m_SerialThread, &SerialThread::finished, m_SerialThread, &SerialThread::deleteLater);
+
+//4、串口发送数据：
+QByteArray strBeepCmd = QByteArray::fromRawData("\nB\r", 3);
+m_SerialThread->WriteCom(strBeepCmd);
+
+//5、串口接收数据
+emit sigRecvOneFrame(QByteArray byteArray);
